e7f33b794b4077baa9ec63a6ac44752f6402befb
unknown <sidhodhank2018@gmail.com>
2022-10-16 20:36:26 +0530

    Retrive: Data From employee Payroll Database

--- src/main/java/jdbc/demo/App.java
+++ src/main/java/jdbc/demo/App.java
@@ -3,15 +3,37 @@ package jdbc.demo;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Enumeration;
 
 public class App {
+	static Connection con;
+
 	public static void main(String[] args) throws SQLException {
+		databaseConnection();
+		ReadDataFromDatabase();
+
+	}
+
+	// List of Drivers Method
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println("   " + driverClass.getClass().getName());
+
+		}
+	}
+
+	// Loading Drivers And Creating Database Connection
+	private static void databaseConnection() throws SQLException {
 		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
 		String userName = "Sidhodhan";
 		String password = "root";
-		Connection connection;
+
+		con = DriverManager.getConnection(jdbcURL, userName, password);
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Driver loadded");
@@ -20,12 +42,11 @@ public class App {
 			throw new IllegalStateException("Cannot find the driver in the classpath!", e);
 		}
 		listDrivers();
-
+		// Connecting to DataBase
 		try {
 			System.out.println("Connecting to database:" + jdbcURL);
-			connection = DriverManager.getConnection(jdbcURL, userName, password);
 
-			System.out.println("Connection is successful!!!!!!" + connection);
+			System.out.println("Connection is successful!!!!!!" + con);
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -33,12 +54,21 @@ public class App {
 
 	}
 
-	private static void listDrivers() {
-		Enumeration<Driver> driverList = DriverManager.getDrivers();
-		while (driverList.hasMoreElements()) {
-			Driver driverClass = (Driver) driverList.nextElement();
-			System.out.println("   " + driverClass.getClass().getName());
-
+	// Method For reading Data from data base
+	private static void ReadDataFromDatabase() {
+		try {
+			Statement statement = con.createStatement();
+			ResultSet result = statement.executeQuery("Select * from employee_payroll_p");
+			System.out.println("Data From Employee Payroll table.");
+			while (result.next()) {
+				System.out.println("ID: " + result.getInt("id"));
+				System.out.println("Name: " + result.getString("Name"));
+				System.out.println("Gender: " + result.getString("gender"));
+				System.out.println("Starting Date: " + result.getString("start") + "\n");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+
 	}
 }
